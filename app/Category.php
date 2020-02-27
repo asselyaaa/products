4a3fafdcb77d124969628d0c068d1f0365f3423d
Assel <salavatova.assel@gmail.com>
2020-02-27 21:51:36 +0600

    refactor(category): add category form, childrenRecursive, parents

--- app/Category.php
+++ app/Category.php
@@ -8,13 +8,29 @@ class Category extends Model
 {
     protected $fillable = ['name', 'parent_id'];
 
-    public function parents()
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
     {
         return $this->hasMany(Category::class, 'parent_id');
     }
 
-    public function childrenCategories()
+    public function childrenRecursive()
     {
-        return $this->hasMany(Category::class, 'parent_id')->with('parents');
+        return $this->children()->with('childrenRecursive');
+    }
+
+    public function parents()
+    {
+        $parents = [];
+        $parent = $this->parent;
+        while (!empty($parent)) {
+            array_push($parents, $parent);
+            $parent = $parent->parent;
+        }
+        return $parents;
     }
 }
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Category;
-use Illuminate\Http\Request;
+use App\Http\Requests\CategoryForm;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:api')->except('index', 'show');
+        $this->authorizeResource(Category::class, 'product');
     }
 
     public function index()
@@ -19,7 +19,7 @@ class CategoryController extends Controller
         $categories = Cache::get('categories');
         if (!$categories) {
             $categories = Category::whereNull('parent_id')
-                ->with('childrenCategories')
+                ->with('childrenRecursive')
                 ->get();
             Cache::put('categories', $categories, 3600*6);
         }
@@ -27,17 +27,8 @@ class CategoryController extends Controller
         return response()->json(compact('categories'));
     }
 
-    public function store(Request $request)
+    public function store(CategoryForm $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'parent_id' => 'nullable|exists:categories,id'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors());
-        }
-
         $category = Category::create($request->all());
         Cache::forget('categories');
 
@@ -49,17 +40,8 @@ class CategoryController extends Controller
         return response()->json(compact('category'));
     }
 
-    public function update(Request $request, Category $category)
+    public function update(CategoryForm $request, Category $category)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'parent_id' => 'nullable|exists:categories,id'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors());
-        }
-
         $category->update($request->all());
         Cache::forget('categories');
 
